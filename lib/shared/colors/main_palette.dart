1c5f609e2b50a338ba89cff1f0f2c13aa1f7917a
ojpro <contact@ojpro.me>
2024-01-07 12:08:42 +0100

    feat(colors): color palette class

---
+++ lib/shared/colors/main_palette.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class MainPalette {
+  static Color primary = const Color(0xFF1A1F38);
+  static Color secondary = const Color(0xFF0A0E21);
+  static Color blue = const Color(0xFF2164d9);
+  static Color greyBackground = const Color(0xFF424554);
+}
