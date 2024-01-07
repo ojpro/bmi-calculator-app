5403592c0cf0de748972dd6c7f451e1f317226c8
ojpro <contact@ojpro.me>
2024-01-07 13:43:19 +0100

    feat(models): add calculate bmi score

--- lib/models/user_info.dart
+++ lib/models/user_info.dart
@@ -3,4 +3,9 @@ class UserInfo{
   static int weight = 64;
   static double height = 165.0;
   static bool isMale = true;
+
+  static double calculateBMIScore() {
+    double heightInMeter = height / 100;
+    return weight /(heightInMeter * heightInMeter);
+  }
 }
