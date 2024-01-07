4d523bf6e67a29fec167d9b614334b27637e76a9
ojpro <contact@ojpro.me>
2024-01-07 13:45:43 +0100

    feat(layout): add leading slot

--- lib/shared/layouts/default_layout.dart
+++ lib/shared/layouts/default_layout.dart
@@ -2,11 +2,17 @@ import 'package:bmi_calculator/shared/components/default_text.dart';
 import 'package:flutter/material.dart';
 import '../colors/main_palette.dart';
 
-Widget defaultLayout({required String title, required Widget slot}) => SafeArea(
+Widget defaultLayout({
+  required String title,
+  required Widget slot,
+  Widget? leading,
+}) =>
+    SafeArea(
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: MainPalette.primary,
           automaticallyImplyLeading: false,
+          leading: leading,
           title: defaultText(
             text: title,
             fontSize: 20,
