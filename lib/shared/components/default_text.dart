84e3e455c0cbc600b7cc24ceb21260ab3c12b8b9
ojpro <contact@ojpro.me>
2024-01-07 13:44:30 +0100

    ref(component): set w500 as default font weight

--- lib/shared/components/default_text.dart
+++ lib/shared/components/default_text.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 Widget defaultText({
   required String text,
   Color color = Colors.white,
-  FontWeight fontWeight = FontWeight.bold,
+  FontWeight fontWeight = FontWeight.w500,
   double fontSize = 16.0,
+  TextAlign textAlign = TextAlign.start,
 }) =>
     Text(
       text,
+      textAlign: textAlign,
       style: TextStyle(
         color: color,
         fontSize: fontSize,
