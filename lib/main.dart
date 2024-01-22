13473405900b2a7d39f7a4ecca3c7703be03a050
ojpro <contact@ojpro.me>
2024-01-22 19:33:43 +0100

    feat(packages): added bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:bloc/bloc.dart';
 import 'package:bmi_calculator/src/app.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+import 'core/bloc/bloc_observer.dart';
+
+void main() {
+  Bloc.observer = MyBlocObserver();
+
   return runApp(const BMIApp());
 }
